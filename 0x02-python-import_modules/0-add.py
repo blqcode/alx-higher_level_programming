d2a1e137f6550b8073b53af3eb0a6f4bac7ad15e
blqcode <ertalb364@gmail.com>
2023-12-01 01:25:50 +0200

    b

--- 0x02-python-import_modules/0-add.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-if __name__ == "__main__":
-    """imports  function add from a file and prints
-    the result of the addition 1 + 2 = 3
-    """
-    from add_0 import add
-    a = 1
-    b = 2
-    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
